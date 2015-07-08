0b778fc3a53fc161787b8b78eb733623b0a313fb
ColinStranc <colinstranc@outlook.com>
2015-07-07 22:28:02 -0500

    Created MvcApplication

---
+++ MockDraft.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MockDraft.Web.MvcApplication" Language="C#" %>
---
+++ MockDraft.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MockDraft.Web.Startup))]
+namespace MockDraft.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
